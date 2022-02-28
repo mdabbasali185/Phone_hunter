672406ef4bbf87edeacf99b1291d054dfc358184
Md Abbas Ali <mdabbasali185@gmail.com>
2022-03-01 01:41:29 +0600

    single phone information

--- js/main.js
+++ js/main.js
@@ -19,10 +19,32 @@ const showResult = (info) => {
               <h6 class="card-text">
                ${singleElement.brand}
               </h6> 
-              <div class="mt-auto"><button  class="btn btn-click">details</button></div>
+              <div class="mt-auto"><button onclick="showSingleInfo('${singleElement.slug}')" class="btn btn-click">details</button></div>
             </div>
           </div>
         `;
     details.appendChild(div);
   });
+};
+const showSingleInfo = (singleInfo) => {
+  const url = ` https://openapi.programming-hero.com/api/phone/${singleInfo}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => phoneDetails(data.data));
+};
+const phoneDetails = (singlePhoneInfo) => {
+  const singlePhone = document.getElementById("single-phone");
+  const div = document.createElement("div");
+  div.classList.add("row");
+  div.innerHTML = `
+          <div class="col-lg-6">
+            <img src="${singlePhoneInfo.image}" class="img-fluid rounded-start" alt="..." />
+          </div>
+          <div class="col-lg-6">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+            </div>
+          </div>
+`;
+  singlePhone.appendChild(div);
 };
